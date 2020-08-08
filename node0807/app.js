a7245105fc4fe26bc44554d7a679c31aef7febf8
LongZhiping <longzhiping@twsz.com>
2020-08-08 11:37:21 +0800

    更新作业

--- node0807/app.js
+++ node0807/app.js
@@ -2,17 +2,16 @@ let http = require('http')
 let fs = require('fs')
 
 let server = http.createServer((request, response) => {
-    console.log('收到一个请求')
-
     let url = request.url
     let content = ''
 
-    if (url.startsWith('/static')) {
-        content = fs.readFileSync('.' + url)
+    if (url.startsWith('/kkb')) {
+        url = url.replace(/^\/kkb/g, '/static')
+        content = fs.readFileSync(__dirname + url)
         response.write(content)
     } else if (url === '/') {
         content = fs.readFileSync('./template/index.html')
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        // response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(content)
     }
 
