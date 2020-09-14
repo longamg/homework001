786cd7da030a68dcfe6bbdec26a4c9da0973bab0
longamg <1441378370@qq.com>
2020-09-14 22:39:08 +0800

    0914

---
+++ 0914-vue-05/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>这里是Home组件</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Home",
+		data() {
+			return {
+				msg: "",
+			};
+		},
+	};
+</script>
+
+<style scoped>
+</style>
---
+++ 0914-vue-05/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h1>这里是Login组件</h1>
+		<button @click="login">Login</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Login",
+		data() {
+			return {
+				msg: "",
+			};
+		},
+		methods: {
+			login() {
+				this.$router.push({
+					name: "Home",
+				});
+			},
+		},
+	};
+</script>
+
+<style scoped>
+</style>
